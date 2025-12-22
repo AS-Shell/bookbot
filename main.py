b0177e68c63619fee5825d1f2b7c5c544edd3ff5
AS-Shell <alexander.shell@outlook.com>
2025-12-22 18:47:29 +0100

    Task: read file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    path_to_file = "/home/alex/workspace/github.com/boot.dev/bookbot/books/frankenstein.txt"
+    print(get_book_text(path_to_file))
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    print(file_contents)
+
+main()
