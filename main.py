46988acad90149d78c214edcda097886e4c1d7e1
AS-Shell <alexander.shell@outlook.com>
2025-12-25 14:23:27 +0100

    Added print report

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from stats import word_counter
+from stats import count_characters
+from stats import char_list
+from stats import sort_list
 
 def main():
-    file = "/home/alex/workspace/github.com/boot.dev/bookbot/books/frankenstein.txt"
+    file = "books/frankenstein.txt"
     file_contents = get_book_text(file)
     words_count = word_counter(file_contents)
-    print(f"Found {words_count} total words")
+    characters_dic = count_characters(file_contents)
+    character_list = char_list(characters_dic)
+
+
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {file}...")
+    print ("----------- Word Count ----------")
+    print (f"Found {words_count} total words")
+    print ("--------- Character Count -------")
+    character_list.sort(reverse=True, key=sort_list)
+    for item in character_list:
+        k = item["char"]
+        v = item["num"]
+        print(f"{k}: {v}")
+    print ("============= END ===============")
+
 
 def get_book_text(file):
     with open(file) as f:
--- stats.py
+++ stats.py
@@ -4,3 +4,29 @@ def word_counter(file_contents):
     for word in words:
         counter +=1
     return counter
+
+def count_characters(file_contents):
+    lower_string = file_contents.lower()
+    char = {}
+
+    for string in lower_string:
+        if string in char:
+            char[string] += 1
+        else : 
+            char[string] = 1 
+    return char
+
+def char_list(characters_dic):
+    list = []
+    for key, value in characters_dic.items():
+        if key.isalpha(): 
+            list.append (
+                {
+                    "char" : key,
+                    "num"  : value
+                }
+            )
+    return list
+
+def sort_list(character_list):
+    return character_list["num"]
